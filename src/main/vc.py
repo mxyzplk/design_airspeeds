8c72dc53ba8d4a82e4e4cd13d75764be5cd04a99
mxyzplk <rafael.pieslak@gmail.com>
2023-07-01 19:22:48 +0100

    vc setting method

--- src/main/vc.py
+++ src/main/vc.py
@@ -2,6 +2,7 @@ import numpy as np
 import aircraft
 import standard_atmosphere
 
+
 class Vc:
     def __init__(self):
 
@@ -10,8 +11,13 @@ class Vc:
         self.vc = np.empty(int(self.ac.ceiling_f00) + 1)
 
         Vc.get_vc(self)
+
     def get_vc(self):
         for i in range(len(self.altitudes)):
             self.altitudes[i] = i
-            speed_eas = standard_atmosphere.Airspeed(self.altitudes[i], self.ac.vc[0], "EAS", 0)
-            speed_mach
+            speed_eas = standard_atmosphere.Airspeed(self.altitudes[i], float(self.ac.vc[0]), "CAS", self.ac.disa)
+            speed_mach = standard_atmosphere.Airspeed(self.altitudes[i], float(self.ac.vc[1]), "Mach", self.ac.disa)
+            if speed_eas.mach > float(self.ac.vc[1]):
+                self.vc[i] = speed_mach.cas
+            else:
+                self.vc[i] = speed_eas.cas
